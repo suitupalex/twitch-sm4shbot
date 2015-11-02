fdd9d4920ef4baf23b2cfaa05232e689ee72e3e0
Alexander Martin <alex@suitupalex.com>
2015-11-02 14:12:31 -0600

    socket: Added socket server.

--- sm4shbot.js
+++ sm4shbot.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const Sm4shClient = require('./lib/Sm4shClient')
+const Sm4shSocket = require('./lib/Sm4shSocket')
 
 const client = new Sm4shClient({
   channels: ['#suitupalex']
 })
 
+const socket = new Sm4shSocket({
+  client: client
+})
+
 client.connect()
+socket.listen(process.env.PORT)
